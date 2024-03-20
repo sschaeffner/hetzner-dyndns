3b23481381922286d5886728e73eefe88d600a9f
Simon Schäffner <simon.schaeffner@googlemail.com>
2024-03-20 12:34:18 +0100

    check return code of curl before taking using its result

--- hetzner.py
+++ hetzner.py
@@ -108,8 +108,10 @@ class HetznerIp:
 
     def get_ip4(self):
         process = subprocess.run(["curl", "-4", self.url], capture_output=True)
+        process.check_returncode()
         return process.stdout.decode("utf-8").strip()
 
     def get_ip6(self):
         process = subprocess.run(["curl", "-6", self.url], capture_output=True)
+        process.check_returncode()
         return process.stdout.decode("utf-8").strip()
